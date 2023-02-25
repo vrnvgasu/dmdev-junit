95d9918f076ee0d74da52242a504c02acf32b140
dmitrii sh <dmitrii@sh.com>
2023-02-25 22:57:55 +0300

    4 Test Lifecycle

--- src/main/java/ru/edu/service/UserService.java
+++ src/main/java/ru/edu/service/UserService.java
@@ -1,13 +1,19 @@
 package ru.edu.service;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import ru.edu.dto.User;
 
 public class UserService {
 
+	private final List<User> users = new ArrayList<>();
+
 	public List<User> getAll() {
-		return Collections.emptyList();
+		return users;
+	}
+
+	public boolean add(User user) {
+		return users.add(user);
 	}
 
 }
--- src/test/java/ru/edu/service/UserServiceTest.java
+++ src/test/java/ru/edu/service/UserServiceTest.java
@@ -1,16 +1,63 @@
 package ru.edu.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import ru.edu.dto.User;
+
+// @TestInstance - жизненный цикл теста
+// PER_METHOD задан по умолчанию (можно не указывать). Объект создается при каждом методе
+@TestInstance(Lifecycle.PER_METHOD)
 class UserServiceTest {
+
+	private UserService userService;
+
+	@BeforeAll
+	static void init() { // название неважно
+	// void init() { можно не статический для Lifecycle.PER_CLASS
+		System.out.println("Before all: ");
+	}
+
+	@BeforeEach
+	void prepare() { // название неважно
+		System.out.println("Before each: " + this);
+		userService = new UserService();
+	}
 
 	@Test
 	// название должно явно говорить, что происходит
 	void usersEmptyIfNoUserAdded() {
-		var userService = new UserService();
+		System.out.println("Test1: " + this);
+
 		var users = userService.getAll();
 		assertTrue(users.isEmpty(), () -> "User list should be empty");
+	}
+
+	@Test
+	void usersSizeIfUserAdded() {
+		System.out.println("Test2: " + this);
+		userService.add(new User());
+		userService.add(new User());
+		var users = userService.getAll();
+		assertEquals(2, users.size());
+	}
+
+	@AfterEach
+	void deleteDataFromDatabase() {
+		System.out.println("After each: " + this);
+	}
+
+	@AfterAll
+	static void closeConnectionPool() {
+		// void closeConnectionPool() { можно не статический для Lifecycle.PER_CLASS
+		System.out.println("After all: ");
 	}
 
 }
